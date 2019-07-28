aede521fbc8b1fd08495446be8d6bbce7e044c30
anouard24 <anouar.fadili.3@gmail.com>
2019-07-28 19:03:43 +0100

    Migrate game2.py to Python3.

--- src/game2.py
+++ src/game2.py
@@ -19,18 +19,18 @@ def getXY_game(game):
 
 	uo1 = [game[0][0][0]-game[0][1][0],1*game[0][1][0]]
 	uf1 = [game[1][0][0]-game[1][1][0],1*game[1][1][0]]
-	print "Les utilites des joueurs :"
-	print "U(j1,s1) =",print_eq(uo1,"q")
-	print "U(j1,s2) =",print_eq(uf1,"q")
+	print("Les utilites des joueurs :")
+	print("U(j1,s1) = "+print_eq(uo1,"q"))
+	print("U(j1,s2) = "+print_eq(uf1,"q"))
 
 	uf2 = [game[0][1][1]-game[1][1][1],1*game[1][1][1]]
 	uo2 = [game[0][0][1]-game[1][0][1],1*game[1][0][1]]
-	print "-"*40
-	print "U(j2,s1) =",print_eq(uo2,"r")
-	print "U(j2,s2) =",print_eq(uf2,"r")
+	print("-"*40)
+	print("U(j2,s1) = "+print_eq(uo2,"r"))
+	print("U(j2,s2) = "+print_eq(uf2,"r"))
 
 	u_indif_1 = [uo1[0]-uf1[0] , uo1[1]-uf1[1]]
-	print "(U1)'=",u_indif_1
+	print("(U1)'= "+str(u_indif_1))
 
 	
 	x2 = np.linspace(0,1,5000)
@@ -48,7 +48,7 @@ def getXY_game(game):
 	y1 = np.linspace(0,1,5000)
 	
 	u_indif_2 = [uo2[0]-uf2[0] , uo2[1]-uf2[1]]
-	print "(U2)'=",u_indif_2
+	print("(U2)'= "+str(u_indif_2))
 
 	for i,yi in enumerate(y1):
 		if u_indif_2[0]*yi + u_indif_2[1] > 0:
@@ -59,15 +59,15 @@ def getXY_game(game):
 
 	ena,enb = (-float(u_indif_1[1])/float(u_indif_1[0]),-float(u_indif_2[1])/float(u_indif_2[0]))
 	if(ena>=0 and ena<=1 and enb>=0 and enb<=1):
-		print "Equilibre de Nash en strategie mixte :(",-(u_indif_1[1]),"/",(u_indif_1[0]),"  ,  ",-(u_indif_2[1]),"/",(u_indif_2[0]),")"
+		print("Equilibre de Nash en strategie mixte :("+str(-(u_indif_1[1]))+"/"+str(u_indif_1[0])+"  ,  "+str(-(u_indif_2[1]))+"/"+str(u_indif_2[0])+")")
 	else:
-		print "Pas d'equilibre de Nash mixte"
+		print("Pas d'equilibre de Nash mixte")
 	return x1,y1,x2,y2
 
 def tracer(game):
-	print "Le tableau du jeu :"
+	print("Le tableau du jeu :")
 	for d in game:
-		print d
+		print(d)
 
 	x1,y1,x2,y2 = getXY_game(game)
 	plt.plot(x1, y1,".-")
